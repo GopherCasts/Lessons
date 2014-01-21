2e6be294bbf10e2c02b126810abb8b71b90eb10d
Jeremy Saenz <jeremy.saenz@gmail.com>
2014-01-20 18:05:18 -0800

    Formatted the main.go file

--- lesson2/main.go
+++ lesson2/main.go
@@ -3,6 +3,6 @@ package main
 import "fmt"
 
 func main() {
-  fmt.Println("To run each example, cd into the example directory and run the go file:")
-  fmt.Println("\n\tcd variables && go build && ./variables\n")
+	fmt.Println("To run each example, cd into the example directory and run the go file:")
+	fmt.Println("\n\tcd variables && go build && ./variables\n")
 }

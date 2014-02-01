c147996df40ea79614d67df86a169cc82731a74b
Nate Beck <nate@zaalabs.com>
2014-01-31 16:51:11 -0800

    Basic search implemented

--- lesson4/main.go
+++ lesson4/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"database/sql"
+	"fmt"
 	"github.com/codegangsta/martini"
 	_ "github.com/lib/pq"
 	"log"
@@ -22,32 +23,67 @@ func SetupDB() *sql.DB {
 	return db
 }
 
+func FormatBook(title, author, description string) string {
+
+	return fmt.Sprintf("Title: %s\nAuthor: %s\nDescription: %s\n\n", title, author, description)
+}
+
 func main() {
 	m := martini.Classic()
 
 	m.Map(SetupDB())
 
 	m.Get("/", func(db *sql.DB) string {
-		var (
-			id int
-			title string
-			author string
-			description string
-			output string
-		)
 
-		rows, err := db.Query("SELECT id, title, author, description FROM books")
+		var title, author, description, output string
+
+		rows, err := db.Query("SELECT title, author, description FROM books")
 		if err != nil {
 			log.Fatal(err)
 		}
 		defer rows.Close()
 
 		for rows.Next() {
-			err := rows.Scan(&id, &title, &author, &description)
+			err := rows.Scan(&title, &author, &description)
 			if err != nil {
 				log.Fatal(err)
 			}
-			output += title + "\n"
+			output += FormatBook(title, author, description)
+		}
+
+		if output == "" {
+			output = "No Books Found!"
+		}
+
+		return output
+	})
+
+	m.Get("/:query", func(db *sql.DB, params martini.Params) string {
+
+		var title, author, description, output string
+
+		queryString := "SELECT title, author, description "
+		queryString += "FROM books "
+		queryString += "WHERE title ILIKE '%" + params["query"] + "%' "
+		queryString += "OR author ILIKE '%" + params["query"] + "%' "
+		queryString += "OR description ILIKE '%" + params["query"] + "%'"
+
+		rows, err := db.Query(queryString)
+		if err != nil {
+			log.Fatal(err)
+		}
+		defer rows.Close()
+
+		for rows.Next() {
+			err := rows.Scan(&title, &author, &description)
+			if err != nil {
+				log.Fatal(err)
+			}
+			output += FormatBook(title, author, description)
+		}
+
+		if output == "" {
+			output = "No Books Found!"
 		}
 
 		return output

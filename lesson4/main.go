3d83c418033fd0d9b0d8d4a09d437a75a9e3473f
Jeremy Saenz <jeremy.saenz@gmail.com>
2014-02-01 11:44:24 -0800

    Cleaned up the SQL code.
            - sql.Query is now using a param
            - One endpoint to rule them all
            - Multiline string
            - fmt.Fprintf on response writer to avoid string buf

--- lesson4/main.go
+++ lesson4/main.go
@@ -1,8 +1,9 @@
 package main
 
 import (
-	"database/sql"
 	"fmt"
+	"database/sql"
+	"net/http"
 	"github.com/codegangsta/martini"
 	_ "github.com/lib/pq"
 )
@@ -13,78 +14,33 @@ func SetupDB() *sql.DB {
 		panic(err)
 	}
 
-	// Does the Database Exist?
-	err = db.Ping()
-	if err != nil {
-		panic(err)
-	}
-
 	return db
-}
-
-func FormatBook(title, author, description string) string {
-	return fmt.Sprintf("Title: %s\nAuthor: %s\nDescription: %s\n\n", title, author, description)
 }
 
 func main() {
 	m := martini.Classic()
-
 	m.Map(SetupDB())
 
-	m.Get("/", func(db *sql.DB) string {
-
-		var title, author, description, output string
-
-		rows, err := db.Query("SELECT title, author, description FROM books")
+	m.Get("/", func(db *sql.DB, r *http.Request, rw http.ResponseWriter) {
+		search := "%" + r.URL.Query().Get("search") + "%"
+		rows, err := db.Query(`SELECT title, author, description 
+                           FROM books 
+                           WHERE title ILIKE $1
+                           OR author ILIKE $1
+                           OR description ILIKE $1;`, search)
 		if err != nil {
 			panic(err)
 		}
 		defer rows.Close()
 
+		var title, author, description string
 		for rows.Next() {
 			err := rows.Scan(&title, &author, &description)
 			if err != nil {
 				panic(err)
 			}
-			output += FormatBook(title, author, description)
+			fmt.Fprintf(rw, "Title: %s\nAuthor: %s\nDescription: %s\n\n", title, author, description)
 		}
-
-		if output == "" {
-			output = "No Books Found!"
-		}
-
-		return output
-	})
-
-	m.Get("/:query", func(db *sql.DB, params martini.Params) string {
-
-		var title, author, description, output string
-
-		queryString := "SELECT title, author, description "
-		queryString += "FROM books "
-		queryString += "WHERE title ILIKE '%" + params["query"] + "%' "
-		queryString += "OR author ILIKE '%" + params["query"] + "%' "
-		queryString += "OR description ILIKE '%" + params["query"] + "%'"
-
-		rows, err := db.Query(queryString)
-		if err != nil {
-			panic(err)
-		}
-		defer rows.Close()
-
-		for rows.Next() {
-			err := rows.Scan(&title, &author, &description)
-			if err != nil {
-				panic(err)
-			}
-			output += FormatBook(title, author, description)
-		}
-
-		if output == "" {
-			output = "No Books Found!"
-		}
-
-		return output
 	})
 
 	m.Run()

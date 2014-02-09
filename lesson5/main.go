fda8fab94822dd0735a6c3e4f1ad0afe91f639e3
Jeremy Saenz <jeremy.saenz@gmail.com>
2014-02-09 15:13:42 -0800

    Adding Lesson 5

---
+++ lesson5/main.go
@@ -0,0 +1,57 @@
+package main
+
+import (
+	"database/sql"
+	"github.com/codegangsta/martini"
+	_ "github.com/lib/pq"
+	"github.com/martini-contrib/render"
+	"net/http"
+)
+
+type Book struct {
+	Title       string
+	Author      string
+	Description string
+}
+
+func SetupDB() *sql.DB {
+	db, err := sql.Open("postgres", "dbname=lesson4 sslmode=disable")
+	PanicIf(err)
+	return db
+}
+
+func PanicIf(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func main() {
+	m := martini.Classic()
+	m.Map(SetupDB())
+	m.Use(render.Renderer(render.Options{
+		Layout: "layout",
+	}))
+
+	m.Get("/", func(ren render.Render, r *http.Request, db *sql.DB) {
+		searchTerm := "%" + r.URL.Query().Get("search") + "%"
+		rows, err := db.Query(`SELECT title, author, description FROM books
+                           WHERE title ILIKE $1
+                           OR author ILIKE $1
+                           OR description ILIKE $1`, searchTerm)
+		PanicIf(err)
+		defer rows.Close()
+
+		books := []Book{}
+		for rows.Next() {
+			book := Book{}
+			err := rows.Scan(&book.Title, &book.Author, &book.Description)
+			PanicIf(err)
+			books = append(books, book)
+		}
+
+		ren.HTML(200, "books", books)
+	})
+
+	m.Run()
+}
